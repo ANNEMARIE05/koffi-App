65a2b89910e2b1a6318f7b5b9a123ded10ce68fd
ANNEMARIE05 <annemarieagbonou@gmail.com>
2024-08-30 03:22:47 +0200

    first commit

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,21 +11,7 @@ const Contact = () => {
 
   return (
     <div className="p-8">
-      <h2 className="text-3xl font-bold mb-4">Votre Panier</h2>
-      <ul className="space-y-4">
-        {cartItems.map(item => (
-          <li key={item.id} className="border p-4 rounded-md shadow-sm flex justify-between items-center">
-            <span>{item.name} (x{item.quantity})</span>
-            <span>${item.price * item.quantity}</span>
-          </li>
-        ))}
-      </ul>
-      <div className="mt-8">
-        <p className="text-xl font-bold">Total : ${totalPrice}</p>
-        <button className="mt-4 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600">
-          Passer la Commande
-        </button>
-      </div>
+      <h2 className="text-3xl font-bold mb-4">De quoi avez-vous besoin</h2>
     </div>
   );
 };
